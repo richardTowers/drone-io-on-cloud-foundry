d2d09fca5691385a14f6d0319d13211b0844a08e
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-12-02 12:14:54 +0000

    More robust way of getting the postgres uri

--- start.sh
+++ start.sh
@@ -4,7 +4,8 @@ set -e -u -o pipefail
 
 cf push --no-start --vars-file .cf-vars.yml
 
-DRONE_DATABASE_DATASOURCE=$(cf env drone | awk -F '"' '/postgres:/ { print $4 }')
+app_guid=$(cf app drone --guid)
+DRONE_DATABASE_DATASOURCE=$(cf curl "/v2/apps/$app_guid/env" | jq -r '.system_env_json.VCAP_SERVICES.postgres[].credentials.uri')
 
 cf set-env drone DRONE_DATABASE_DATASOURCE "$DRONE_DATABASE_DATASOURCE"
 
